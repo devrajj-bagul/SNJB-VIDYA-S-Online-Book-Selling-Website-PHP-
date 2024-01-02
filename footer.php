089d46d0f528b682f6c9bd346dbe8ab9db3f334f
Devraj Bagul <133137679+devrajj-bagul@users.noreply.github.com>
2024-01-02 15:37:08 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,33 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91 9284438720 </p>
+         <p> <i class="fas fa-phone"></i> +91 9130747665 </p>
+         <p> <i class="fas fa-envelope"></i> malegaonshoot41@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> malegaon, Nashik - 423203 </p>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span> SNJB Vidya's </span> </p>
+
+</section>
